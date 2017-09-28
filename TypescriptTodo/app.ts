b6fd9e39d22f7b1d5e1933a01be1d8f0b08edc8a
Kevin Cartwright <kevincart@gmail.com>
2017-09-29 12:08:25 +1300

    studying basic typescript wip

---
+++ TypescriptTodo/app.ts
@@ -0,0 +1,9 @@
+var container = document.getElementById('container');
+
+var todo = {
+    id: 123,
+    name: "Pick up drycleaning",
+    completed: true
+}
+
+var displayName = `Todo #${todo.id}`;
