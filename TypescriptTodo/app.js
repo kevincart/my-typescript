d948473b312bc794e1e5b12ebbb9b20270f060cf
Kevin Cartwright <kevincart@gmail.com>
2017-10-03 21:31:40 +1300

    studying TS, wip

--- TypescriptTodo/app.js
+++ TypescriptTodo/app.js
@@ -4,4 +4,22 @@ var todo = {
     name: "Pick up drycleaning",
     completed: true
 };
-var displayName = "Todo #" + todo.id;
+container.innerHTML = "\n<div todo='" + todo.id + "' class=\"list-group-item}\">\n<i class=\"" + (todo.completed ? " " : "hidden") + " text-success glyphicon glyphicon-ok\"></i>\n<span class=\"name\">" + todo.name + "</span>\n</div>\n";
+// Destructuring example
+var array = [123, "Pick up drycleaning", false];
+var id = array[0], title = array[1], completed = array[2];
+// Another destructuring example
+var a = 1;
+var b = 5;
+_a = [b, a], a = _a[0], b = _a[1]; // reverses values
+// Computed Property example:
+var osPrefix = 'os_';
+var support = (_b = {},
+    _b[osPrefix + 'Windows'] = isSupported('Windows'),
+    _b[osPrefix + 'iOS'] = isSupported('iOS'),
+    _b[osPrefix + 'Android'] = isSupported('Android'),
+    _b);
+function isSupported(os) {
+    return Math.random() >= 0.5;
+}
+var _a, _b;
--- TypescriptTodo/app.ts
+++ TypescriptTodo/app.ts
@@ -6,4 +6,32 @@ var todo = {
     completed: true
 }
 
-var displayName = `Todo #${todo.id}`;
+container.innerHTML = `
+<div todo='${todo.id}' class="list-group-item}">
+<i class="${ todo.completed ? " ": "hidden"} text-success glyphicon glyphicon-ok"></i>
+<span class="name">${ todo.name }</span>
+</div>
+`
+
+// Destructuring example
+var array = [123, "Pick up drycleaning", false];
+var [id, title, completed] = array;
+
+// Another destructuring example
+var a = 1;
+var b = 5;
+
+[a,b] = [b,a]; // reverses values
+
+// Computed Property example:
+const osPrefix = 'os_';
+
+var support = {
+    [osPrefix + 'Windows']: isSupported('Windows'),
+    [osPrefix + 'iOS']: isSupported('iOS'),
+    [osPrefix + 'Android']: isSupported('Android')
+}
+
+function isSupported(os) {
+    return Math.random() >= 0.5;
+}
